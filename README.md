536dca2a764a47e0ab2e453a602646d67d30dc8f
JonasSiuksteris <jonasjonas32@gmail.com>
2018-05-31 01:26:08 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Darbas5
+﻿# Darbas5
 Asociatyvūs konteineriai
+
+Užduotis realizuota. Programa išveda žodžius, kurie kartojasi daugiau nei 2 kartus, bei jų eilutes.
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <sstream> /// https://stackoverflow.com/questions/18318980/taking-input-of-a-string-word-by-word
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int lineNumber = 1;
+    string line, word = "";
+    map<string, size_t>  Count;
+    map<string, map<size_t, size_t>> Place;
+
+    ifstream fd("input.txt");
+    while(getline(fd,line))
+    {
+        for(int i = 0; i < line.length(); i++)
+        {
+            if(!(( line[i] >= ' ' && line[i] <= '/' ) || ( line[i] >= ':' && line[i] <= '?' ) || ( line[i] >= '[' && line[i] <= '`' ) || ( line[i] >= '{' && line[i] <= '~' )))
+            {
+                word.push_back(line[i]);
+                while(!(( line[i+1] >= ' ' && line[i+1] <= '/' ) || ( line[i+1] >= ':' && line[i+1] <= '?' ) || ( line[i+1] >= '[' && line[i+1] <= '`' ) || ( line[i+1] >= '{' && line[i+1] <= '~' )) && i+1 < line.length())
+                {
+                    i++;
+                    word.push_back(line[i]);
+                }
+                cout << word << endl;
+                Count[word]++;
+                Place[word][lineNumber]++;
+                word = "";
+            }
+        }
+        lineNumber++;
+    }
+    fd.close();
+
+    ofstream fr("output.txt");
+    for (auto &i : Count)
+        if(i.second > 1)
+        {
+            fr << "Word '" << i.first << "' is repeated " << i.second << " times:" << endl;
+            for (auto &j : Place.at(i.first)){
+                fr << "Line " <<j.first << " - " << j.second << " times" << endl;
+            }
+            fr << endl << endl;
+        }
+
+    return 0;
+
+
+}
